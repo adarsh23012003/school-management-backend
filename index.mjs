cff75a4ffb57a683401aaee0f866c7df5c3cddf2
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-22 11:50:11 +0530

    Database Connected successfully to server

--- index.mjs
+++ index.mjs
@@ -1,4 +1,27 @@
 import app from "./app.mjs";
+import { MongoClient } from "mongodb";
+
+// Connection URL
+const url =
+  "mongodb+srv://School-management:340384792347@cluster0.4jxf50a.mongodb.net/";
+const client = new MongoClient(url);
+// Database Name
+const dbName = "school-management";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("student");
+
+  // the following code examples can be pasted here...
+  const respond = await collection.find({}).toArray();
+  console.log(respond);
+}
+
+main();
+
 app.listen(5000, () => {
   console.log("server started");
 });
