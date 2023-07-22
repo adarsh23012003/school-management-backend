16323df9df82813b6d012f719dbdcf1622c2b62f
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-22 14:03:13 +0530

    sone function not calling

--- index.mjs
+++ index.mjs
@@ -1,12 +1,25 @@
 import app from "./app.mjs";
 import dbConnect from "./Database/dbConnect.mjs";
 
+// ********************* Data getting *********************************
+
 const main = async () => {
   let data = await dbConnect();
   data = await data.find().toArray();
   console.log(data);
 };
-main();
+// main();
+// ********************* Data inserting *********************************
+const insert = async () => {
+  const database = await dbConnect();
+  const result = await database.insertOne({
+    name: "banana",
+    age: "26",
+  });
+  console.log(result);
+};
+// insert();
+
 app.listen(5000, () => {
   console.log("server started is successfully");
 });
