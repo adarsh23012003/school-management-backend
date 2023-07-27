bafdb6a1befe694f2b85f1adc18619b2ed66f1fd
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-27 15:27:06 +0530

    process.env related changes

--- index.mjs
+++ index.mjs
@@ -1,14 +1,13 @@
 import app from "./app.mjs";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 
-const url =
-  "mongodb+srv://School-management:340384792347@cluster0.4jxf50a.mongodb.net/student";
-const port = 5000;
 mongoose
-  .connect(url, {})
+  .connect(process.env.DB_URL, {})
   .then(() => console.log("Connected successfully to server"))
   .catch((error) => console.log(error));
 
-app.listen(port, () => {
-  console.log(`server started on http://localhost:${port}/`);
+app.listen(process.env.PORT, () => {
+  console.log(`server started on http://localhost:${process.env.PORT}/`);
 });
